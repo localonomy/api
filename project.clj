c8820852778bb6dd61fdaec86c1e12e55975ebab
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-26 23:51:57 +0200

    Read response JSON from file

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "API for the Localonomy app"
+  :url "https://github.com/localonomy"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.7.1"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler api.handler/app}
   :profiles
--- src/api/handler.clj
+++ src/api/handler.clj
@@ -1,11 +1,20 @@
 (ns api.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.java.io :as io]
+            [cheshire.core :as json]
+            [ring.middleware.defaults :as middleware]
+            [ring.middleware.json :as middleware.json]
+            [ring.util.response :as response]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] 
+    (response/response
+      (json/decode-stream
+        (io/reader "data/hello.json") true)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes 
+    (middleware.json/wrap-json-response)
+    (middleware/wrap-defaults middleware/api-defaults)))
