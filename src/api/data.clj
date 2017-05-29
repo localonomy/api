7318385f8ed25fd9f39357f866b4f68d7e49435b
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-30 01:42:05 +0200

    Add dishes from 15 countries

--- src/api/data.clj
+++ src/api/data.clj
@@ -7,7 +7,33 @@
     (json/decode-stream
       (io/reader "resources/data/dishes/be.json") true)
     (json/decode-stream
-      (io/reader "resources/data/dishes/cl.json") true)))
+      (io/reader "resources/data/dishes/cl.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/cn.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/do.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/es.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/fr.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/hu.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/it.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/jp.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/kr.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/mx.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/sg.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/th.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/tw.json") true)
+    (json/decode-stream
+      (io/reader "resources/data/dishes/us.json") true)))
 
 (def countries
   (json/decode-stream
