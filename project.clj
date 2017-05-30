e63f5dac5ad90f85520566b29493c9adc3b209e3
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-30 18:22:07 +0200

    Remove environ plugin and hook

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject api "0.1.0"
   :description "API for the Localonomy app"
   :url "https://github.com/localonomy"
-  :min-lein-version "2.7.0"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.7.1"]
                  [compojure "1.5.1" :exclusions [[ring/ring-core]]]
@@ -9,9 +9,7 @@
                  [ring/ring-defaults "0.2.1" :exclusions [[ring/ring-core]]]
                  [ring/ring-jetty-adapter "1.6.1"]
                  [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]
-            [environ/environ.lein "0.3.1"]]
-  :hooks [environ.leiningen.hooks]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler api.handler/app}
   :uberjar-name "api-standalone.jar"
   :profiles {
