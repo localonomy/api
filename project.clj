91a77b4acaff0a73acdc02890c8bee9b357f421b
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-30 17:26:54 +0200

    Deploy to Heroku

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject api "0.1.0-SNAPSHOT"
+(defproject api "0.1.0"
   :description "API for the Localonomy app"
   :url "https://github.com/localonomy"
   :min-lein-version "2.0.0"
@@ -9,6 +9,9 @@
                  [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler api.handler/app}
+  :uberjar-name: "api-standalone.jar"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  {:production {:env {:production true}}}
+  {:uberjar {:aot :all}})
