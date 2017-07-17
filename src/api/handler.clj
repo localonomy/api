995d958e91d8c12c98ff7fe25e6dacc56c23fbc7
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-07-17 17:15:06 -0400

    Allow CORS from Localonomy webapp

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -28,7 +28,7 @@
   (-> app-routes 
     (middleware.json/wrap-json-response)
     (middleware.cors/wrap-cors
-      :access-control-allow-origin [#".*localhost.*"]
+      :access-control-allow-origin [#".*localhost.*", #".*localonomy.herokuapp.com"]
       :access-control-allow-methods [:get :put :post :delete])
     (middleware/wrap-defaults middleware/api-defaults)))
 
